95061165c922f0b67766499669a3b81483544a5b
Amirah Anwar <amirah.anwar@square63.com>
2016-03-09 19:12:12 +0500

    Bootstrap: Added styles through twitter-bootstrap gem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   respond_to :html
 
   def index
-    @products = Product.all
+    @products = Product.order("title").page(params[:page]).per(10)
   end
 
   def show
