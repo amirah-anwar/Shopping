0dc0cffd07233c25e70c9936fcec6b0c3cc2fdbc
Amirah Anwar <amirah.anwar@square63.com>
2016-03-14 18:16:39 +0500

    Deleted unused files

--- db/migrate/20160309093525_add_product_id_to_orders.rb
+++
@@ -1,9 +0,0 @@
-class AddProductIdToOrders < ActiveRecord::Migration
-  def up
-    add_column :orders, :product_id, :integer
-  end
-
-  def down
-    remove_column :orders, :product_id
-  end
-end
